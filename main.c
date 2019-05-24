57fb43c667e3a0f5790f11250673a3a9dc97a9e6
ZoRax-A5 <weixuzong@gmail.comgit pull origin mastergit config --global user.name ZoRax-A5git config --global user.email weixuzong@gmail.com>
2019-05-24 11:28:59 +0800

    version 1.0

--- main.c
+++ main.c
@@ -3,16 +3,17 @@
 #include <math.h>
 #include "acllib.h"
 
-#define length 1210
-#define width 800
-#define MAX 1000000
+#define length 1280
+#define width 800 //define the window size.
+#define MAX 203 //the maximum number of character it can save.
+#define LINEMAX 116 //the maximum number of character one line can hold
 #define deltax 11
-#define deltay 22
+#define deltay 22 //the unit length that the caret moves.
 
-#define caretwidth 2
-#define caretheight 22
+#define caret_width 2
+#define caret_height 22 //the caret's size
 
-#define textsize 20
+#define font_size 20 //font size
 
 #define backspace 8
 #define table 9
@@ -27,57 +28,67 @@
 #define rightarrow 39
 #define Delete 46
 
-void initCaret();
+void DrawReminder();
+void InitCaret();
 void MoveCaret(int dx);
-void scanText(int key, int event);
-void Print(int key,int event);
-void Capslock(int key,int event);
-void Backspace(int key,int event);
-void DeLeTe(int key,int event);
-void LeftArrow(int key,int event);
-void RightArrow(int key,int event);
-void Table(int key,int event);
-void Home(int key,int event);
-void End(int key,int event);
-void Shift(int key,int event);
-void TextConfig(int key,int event);
-void Exit(int key,int event);
-void insertText(int key,int event);
-void deFault(int key,int event);
+void ScanText(int key, int event);
+void Print(int key, int event);
+void Capslock(int key, int event);
+void Backspace(int key, int event);
+void DeLeTe(int key, int event);
+void LeftArrow(int key, int event);
+void RightArrow(int key, int event);
+void Table(int key, int event);
+void Home(int key, int event);
+void End(int key, int event);
+void Shift(int key, int event);
+void TextConfig(int key, int event);
+void Exit(int key, int event);
+void InsertText(int key, int event);
+void Default(int key, int event);
 
-int p = 0,len = 0;
-int x = 0, y = 0, dx = 0;
-int Caps = 1,SHIFT = 1;
-int textcolor = BLACK,textbkcolor = WHITE;
-char ch[MAX] = { 0 };
+int p = 0, len = 0; //p remarks the position of the caret,while len remarks the length of the text.
+int x = 0, y = 0, dx = 0; //the real position of a line's beginning.
+int Caps = 1, SHIFT = 1; //both viriables are used to hold the case sensitivity.
+int textcolor = BLACK, textbkcolor = WHITE; //these two will be COLORREF variables to hold the text color and the background color.
+char ch[MAX] = { 0 }; //hold the text you input.
 
 int Setup()
 {
 	initConsole();
 	initWindow("zwx", DEFAULT, DEFAULT, length, width);
-	initCaret();
-	registerKeyboardEvent(scanText);
+	DrawReminder();
+	InitCaret();
+	registerKeyboardEvent(ScanText);
 	return 0;
 }
-void initCaret()
+void DrawReminder()
 {
-	setCaretSize(caretwidth, caretheight);
+	beginPaint();
+	setPenWidth(2);
+	moveTo(1276, y+1);
+	lineRel(0, caret_height-4);
+	endPaint();
+}
+void InitCaret()
+{
+	setCaretSize(caret_width, caret_height);
 	showCaret();
 	setCaretPos(x, y);
 }
 void MoveCaret(int dx)
 {
-	setCaretSize(caretwidth, caretheight);
+	setCaretSize(caret_width, caret_height);
 	showCaret();
 	setCaretPos(x + dx, y);
 	dx = 0;
 }
-void scanText(int key, int event)
+void ScanText(int key, int event)
 {
 	int j = 0;
-	void(*point[20])(int key,int event) =
+	void(*point[20])(int key, int event) =
 	{
-		Print,Capslock,Backspace,DeLeTe,LeftArrow,RightArrow,Table,Home,End,Shift,TextConfig,Exit,insertText,deFault
+		Print,Capslock,Backspace,DeLeTe,LeftArrow,RightArrow,Table,Home,End,Shift,TextConfig,Exit,InsertText,Default
 	};
 	if (!event)
 	{
@@ -96,36 +107,37 @@ void scanText(int key, int event)
 		else if (key == space) j = 12;
 		else j = 12;
 	}
-	else 
+	else
 	{
 		if (key == shift) j = 9;
 		else j = 13;
 	}
-	(*point[j])(key,event);
+	(*point[j])(key, event);
 }
 void showText()
 {
 	beginPaint();
-	setTextSize(textsize);
+	setTextSize(font_size);
 	setTextColor(textcolor);
 	setTextBkColor(textbkcolor);
 	paintText(x, y, ch);
 	endPaint();
 }
-void Print(int key,int event)
+void Print(int key, int event)
 {
 	int j = 0;
 	for (j = 0;j < len;j++) printf("%c", ch[j]);
 	printf("\n");
 	for (j = 0;j < len;j++) ch[j] = 0;
 	p = 0;dx = 0;len = 0;y += deltay;
-	initCaret();
+	DrawReminder();
+	InitCaret();
 }
-void Capslock(int key,int event)
+void Capslock(int key, int event)
 {
 	Caps *= -1;
 }
-void Backspace(int key,int event)
+void Backspace(int key, int event)
 {
 	int j = 0;
 	if (p != 0)
@@ -137,7 +149,7 @@ void Backspace(int key,int event)
 		MoveCaret(dx);
 	}
 }
-void DeLeTe(int key,int event)
+void DeLeTe(int key, int event)
 {
 	int j = 0;
 	if (p != len)
@@ -149,7 +161,7 @@ void DeLeTe(int key,int event)
 		MoveCaret(dx);
 	}
 }
-void LeftArrow(int key,int event)
+void LeftArrow(int key, int event)
 {
 	if (p)
 	{
@@ -159,7 +171,7 @@ void LeftArrow(int key,int event)
 	}
 
 }
-void RightArrow(int key,int event)
+void RightArrow(int key, int event)
 {
 	if (p != len)
 	{
@@ -168,7 +180,7 @@ void RightArrow(int key,int event)
 		MoveCaret(dx);
 	}
 }
-void Table(int key,int event)
+void Table(int key, int event)
 {
 	int m = 0, n = 0;
 	for (m = 0;m < 4;m++)
@@ -184,174 +196,175 @@ void Table(int key,int event)
 	showText();
 	MoveCaret(dx);
 }
-void Home(int key,int event)
+void Home(int key, int event)
 {
 	dx -= p * deltax;
 	p = 0;
 	MoveCaret(dx);
 }
-void End(int key,int event)
+void End(int key, int event)
 {
 	dx += (len - p)*deltax;
 	p = len;
 	MoveCaret(dx);
 }
-void Shift(int key,int event)
+void Shift(int key, int event)
 {
-	if(!event) SHIFT = -1;
+	if (!event) SHIFT = -1;
 	else SHIFT = 1;
 }
-void TextConfig(int key,int event)
+void TextConfig(int key, int event)
 {
-	switch(key)
+	switch (key)
 	{
-		case 112: textcolor = BLACK; break;
-		case 113: textcolor = RED; break;
-		case 114: textcolor = YELLOW; break;
-		case 115: textcolor = CYAN; break;
-		case 116: textbkcolor = WHITE; break;
-		case 117: textbkcolor = RED; break;
-		case 118: textbkcolor = YELLOW; break;
-		case 119: textbkcolor = CYAN; break;
+	case 112: textcolor = BLACK; break;
+	case 113: textcolor = RED; break;
+	case 114: textcolor = YELLOW; break;
+	case 115: textcolor = CYAN; break;
+	case 116: textbkcolor = WHITE; break;
+	case 117: textbkcolor = RED; break;
+	case 118: textbkcolor = YELLOW; break;
+	case 119: textbkcolor = CYAN; break;
 	}
 	showText();
 }
-void Exit(int key,int event)
+void Exit(int key, int event)
 {
 	exit(0);
 }
-void insertText(int key,int event)
+void InsertText(int key, int event)
 {
+	if (len == LINEMAX) Print(key,event);
 	char temp;
-	int j = 0;
 	if (key == space) temp = key;
 	else if (key >= 48 && key <= 57)
 	{
-		switch(key)
+		switch (key)
 		{
-			case 49:
-			if(SHIFT == 1) temp = '1';
-			else if(SHIFT == -1) temp = '!';
+		case 49:
+			if (SHIFT == 1) temp = '1';
+			else if (SHIFT == -1) temp = '!';
 			break;
-			case 50:
-			if(SHIFT == 1) temp = '2';
-			else if(SHIFT == -1) temp = '@';
+		case 50:
+			if (SHIFT == 1) temp = '2';
+			else if (SHIFT == -1) temp = '@';
 			break;
-			case 51:
-			if(SHIFT == 1) temp = '3';
-			else if(SHIFT == -1) temp = '#';
+		case 51:
+			if (SHIFT == 1) temp = '3';
+			else if (SHIFT == -1) temp = '#';
 			break;
-			case 52:
-			if(SHIFT == 1) temp = '4';
-			else if(SHIFT == -1) temp = '$';
+		case 52:
+			if (SHIFT == 1) temp = '4';
+			else if (SHIFT == -1) temp = '$';
 			break;
-			case 53:
-			if(SHIFT == 1) temp = '5';
-			else if(SHIFT == -1) temp = '%';
+		case 53:
+			if (SHIFT == 1) temp = '5';
+			else if (SHIFT == -1) temp = '%';
 			break;
-			case 54:
-			if(SHIFT == 1) temp = '6';
-			else if(SHIFT == -1) temp = '^';
+		case 54:
+			if (SHIFT == 1) temp = '6';
+			else if (SHIFT == -1) temp = '^';
 			break;
-			case 55:
-			if(SHIFT == 1) temp = '7';
-			else if(SHIFT == -1) temp = '&';
+		case 55:
+			if (SHIFT == 1) temp = '7';
+			else if (SHIFT == -1) temp = '&';
 			break;
-			case 56:
-			if(SHIFT == 1) temp = '8';
-			else if(SHIFT == -1) temp = '*';
+		case 56:
+			if (SHIFT == 1) temp = '8';
+			else if (SHIFT == -1) temp = '*';
 			break;
-			case 57:
-			if(SHIFT == 1) temp = '9';
-			else if(SHIFT == -1) temp = '(';
+		case 57:
+			if (SHIFT == 1) temp = '9';
+			else if (SHIFT == -1) temp = '(';
 			break;
-			case 48:
-			if(SHIFT == 1) temp = '0';
-			else if(SHIFT == -1) temp = ')';
+		case 48:
+			if (SHIFT == 1) temp = '0';
+			else if (SHIFT == -1) temp = ')';
 			break;
 		}
 	}
 	else if (key >= 65 && key <= 90)
 	{
-		if(SHIFT == 1)
+		if (SHIFT == 1)
 		{
-			if(Caps == -1) temp = key;
+			if (Caps == -1) temp = key;
 			else if (Caps == 1) temp = key - 'A' + 'a';
 		}
-		else if(SHIFT == -1)
+		else if (SHIFT == -1)
 		{
-			if(Caps == -1) temp = key - 'A' + 'a';
+			if (Caps == -1) temp = key - 'A' + 'a';
 			else if (Caps == 1) temp = key;
 		}
 	}
 	else if (key >= 186 && key <= 192)
 	{
-		switch(key)
+		switch (key)
 		{
-			case 186:
-			if(SHIFT == 1) temp = ';';
-			else if(SHIFT == -1) temp = ':';
+		case 186:
+			if (SHIFT == 1) temp = ';';
+			else if (SHIFT == -1) temp = ':';
 			break;
-			case 187:
-			if(SHIFT == 1) temp = '=';
-			else if(SHIFT == -1) temp = '+';
+		case 187:
+			if (SHIFT == 1) temp = '=';
+			else if (SHIFT == -1) temp = '+';
 			break;
-			case 188:
-			if(SHIFT == 1) temp = ',';
-			else if(SHIFT == -1) temp = '<';
+		case 188:
+			if (SHIFT == 1) temp = ',';
+			else if (SHIFT == -1) temp = '<';
 			break;
-			case 189:
-			if(SHIFT == 1) temp = '-';
-			else if(SHIFT == -1) temp = '_';
+		case 189:
+			if (SHIFT == 1) temp = '-';
+			else if (SHIFT == -1) temp = '_';
 			break;
-			case 190:
-			if(SHIFT == 1) temp = '.';
-			else if(SHIFT == -1) temp = '>';
+		case 190:
+			if (SHIFT == 1) temp = '.';
+			else if (SHIFT == -1) temp = '>';
 			break;
-			case 191:
-			if(SHIFT == 1) temp = '/';
-			else if(SHIFT == -1) temp = '?';
+		case 191:
+			if (SHIFT == 1) temp = '/';
+			else if (SHIFT == -1) temp = '?';
 			break;
-			case 192:
-			if(SHIFT == 1) temp = '`';
-			else if(SHIFT == -1) temp = '~';
+		case 192:
+			if (SHIFT == 1) temp = '`';
+			else if (SHIFT == -1) temp = '~';
 			break;
 		}
 	}
 	else if (key >= 219 && key <= 222)
 	{
-		switch(key)
+		switch (key)
 		{
-			case 219:
-			if(SHIFT == 1) temp = '[';
-			else if(SHIFT == -1) temp = '{';
+		case 219:
+			if (SHIFT == 1) temp = '[';
+			else if (SHIFT == -1) temp = '{';
 			break;
-			case 220:
-			if(SHIFT == 1) temp = '\\';
-			else if(SHIFT == -1) temp = '|';
+		case 220:
+			if (SHIFT == 1) temp = '\\';
+			else if (SHIFT == -1) temp = '|';
 			break;
-			case 221:
-			if(SHIFT == 1) temp = ']';
-			else if(SHIFT == -1) temp = '}';
+		case 221:
+			if (SHIFT == 1) temp = ']';
+			else if (SHIFT == -1) temp = '}';
 			break;
-			case 222:
-			if(SHIFT == 1) temp = '\'';
-			else if(SHIFT == -1) temp = '"';
+		case 222:
+			if (SHIFT == 1) temp = '\'';
+			else if (SHIFT == -1) temp = '"';
 			break;
 		}
 	}
 	else return;
 	dx += deltax;
 	len++;
-	for (j = len - 1;j > p;j--)
+	for (int j = len - 1;j > p;j--)
 	{
 		ch[j] = ch[j - 1];
 	}
-	ch[j] = temp;
+	ch[p] = temp;
 	p++;
+
 	showText();
 	MoveCaret(dx);
 }
-void deFault(int key,int event) {
+void Default(int key, int event) {
 }
 
